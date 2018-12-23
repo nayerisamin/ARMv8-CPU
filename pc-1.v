acc26a58e875902f422855e3b2fb924f04d36e88
nayerisamin <44843028+nayerisamin@users.noreply.github.com>
2018-12-23 16:04:38 +0330

    delayed

--- pc-1.v
+++ pc-1.v
@@ -3,16 +3,18 @@ module pc(input clk,
     input write,
     input newpc,
     output reg oldpc)
-
+    parameter delay_t=5;
 
 
 always@(posedge clk)
     begin
         if(reset)
+            #delay_t
             begin
                 newpc <= 0;
             end
         else if(write)
+            #delay_t
             begin
                 oldpc <= newpc;
             end             
