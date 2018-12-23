645a1a0f7dfd2ea708c4602018cd49b8efb2e05e
nayerisamin <44843028+nayerisamin@users.noreply.github.com>
2018-12-23 15:56:55 +0330

    Update instructionMemory.v

--- instructionMemory.v
+++ instructionMemory.v
@@ -1,14 +1,23 @@
 module InstructionMemory( 
   input [63:0] ReadAddr,
-  output [31:0] Instruction
+  output reg [31:0] Instruction
   );
   parameter delay_t = 5;
 
-  reg [7:0] Memory [0:63];
+  reg [31:0] Memory [0:63];
   
-  assign Instruction[7:0] = Memory[ReadAddr]; 
-  assign Instruction[15:8] = Memory[ReadAddr + 1];
-  assign Instruction[23:16] = Memory[ReadAddr + 2];
-  assign Instruction[31:24] = Memory[ReadAddr + 3];
+  
+  initial
+
+
+		begin
+			  Memory[0]    = 32'h8b1f03e5;
+				Memory[1]    = 32'hf84000a4;
+				Memory[2]    = 32'h8b040086;
+        Memory[3]    = 32'hf80010a6;
+				end
+         
+	 
+end
 
 endmodule 
