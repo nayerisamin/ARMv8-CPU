d15439135afa91c41a427009aeafc6f34b13f3b4
nayerisamin <44843028+nayerisamin@users.noreply.github.com>
2018-12-23 11:20:02 +0330

    Add files via upload

---
+++ ALU.v
@@ -0,0 +1,26 @@
+module ALU(input [63:0]A,
+input [63:0]B,
+input [3:0]op,
+output reg z,
+output reg [63:0]Y)
+
+  always@(A,B,op)
+    begin
+      case(op)
+      		  4'b0000:Y = A&B;
+		  4'b0001:Y = A|B;
+		  4'b0010:Y = A+B;
+		  4'b0110:Y = A-B;
+	       	  4'b1100:Y = ~(A | B);
+          	  4'b0111:Y =B; 
+      endcase
+      if(Y==0)    
+        begin
+          z <= 1;
+        end
+      else
+        begin
+          z <= 0;
+        end
+    end
+endmodule
---
+++ MUX.v
@@ -0,0 +1,9 @@
+module MUX(output reg [63:0] Y;
+input [63:0] A;
+input [63:0] B;
+input select;
+)
+	parameter delay_t =5;
+		assign #delay_t Y = (select)?A:B;
+			
+endmodule
