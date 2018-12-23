029c1ed22e22e886248c52b84c7fe0b5fee26092
nayerisamin <44843028+nayerisamin@users.noreply.github.com>
2018-12-23 12:29:03 +0330

    read without always

--- regBank.v
+++ regBank.v
@@ -11,19 +11,13 @@ output reg [0:63]readData2)
 parameter delay_t = 5;
 reg [63:0] registers [0:31];
 
-assign readData1 = ()?registers[readReg1]:;
-assign readData2 = ()?registers[readReg2]:;
+assign #delay_t readData1 = ()?registers[readReg1]:;
+assign #delay_t readData2 = ()?registers[readReg2]:;
 
 always @(posedge clk)
-		#delay_t;
 		begin
-
-		readData1 = registers[readReg1];
-    		readData2 = registers[readReg2];
-	
 			if(regWrite == 1)
 			begin 
-			
 				registers[writeReg] <= writeData;
 
 			end	
