c48a9c21968527398dbe883d566c5c7f6b428b41
nayerisamin <44843028+nayerisamin@users.noreply.github.com>
2018-12-28 18:55:55 +0330

    syntax error fixed

--- ALU.v
+++ ALU.v
@@ -2,19 +2,26 @@ module ALU(input [63:0]A,
 input [63:0]B,
 input [3:0]op,
 output reg z,
-output reg [63:0]Y)
+output reg [63:0]Y); //ALU Output
 
   always@(A,B,op)
     begin
       case(op)
-      		  4'b0000:Y = A&B;
-		  4'b0001:Y = A|B;
-		  4'b0010:Y = A+B;
-		  4'b0110:Y = A-B;
-	       	  4'b1100:Y = ~(A | B);
-          	  4'b0111:Y =B; 
-      endcase
-      if(Y==0)    
+      		  4'b0000:
+				Y = A&B;
+		  4'b0001:
+				Y = A|B;
+		  4'b0010:
+				Y = A+B;
+		  4'b0110:
+				Y = A-B;
+	       	  4'b1100:
+				Y = ~(A | B);
+          	  4'b0111:
+				Y =B; 
+     	 // default: ALU_Result = 0; 
+	endcase
+      if(Y==64'b0)    
         begin
           z <= 1;
         end
@@ -23,4 +30,4 @@ output reg [63:0]Y)
           z <= 0;
         end
     end
-endmodule
+   endmodule
