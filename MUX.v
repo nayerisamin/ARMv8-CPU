8bf233315249c710100ffb82162558c226440ea4
nayerisamin <44843028+nayerisamin@users.noreply.github.com>
2018-12-29 16:15:43 +0330

    Update MUX.v

--- MUX.v
+++ MUX.v
@@ -1,7 +1,8 @@
-module MUX(output reg [63:0] Y,
+module MUX(output[63:0] Y,
 input [63:0] A,
 input [63:0] B,
 input select);
 	parameter delay_t =5;
-		assign #delay_t Y = (select)?A:B;
+		 
+assign #delay_t  Y = (select)?A:B;
 endmodule
