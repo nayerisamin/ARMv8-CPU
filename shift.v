57ce2563647e4708b297fdea925b55e04e0351f2
nayerisamin <44843028+nayerisamin@users.noreply.github.com>
2018-12-30 13:39:59 +0330

    shift done

--- shift.v
+++ shift.v
@@ -1,5 +1,5 @@
-module ShiftUnit #(parameter n = 64) (input_data, output_data);
-    input [n - 1 : 0] input_data;
-    output[n - 1 : 0] output_data;
+module ShiftUnit (input_data, output_data);
+    input [63 : 0] input_data;
+    output[63 : 0] output_data;
     assign output_data = input_data << 2;
  endmodule
